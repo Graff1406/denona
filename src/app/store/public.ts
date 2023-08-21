b2cfd9f2c2d36ab8e35f82c78daecca7eb5e0e5e
avtandil <shainidze@gmail.com>
2023-08-21 16:53:30 +0300

    feat: add RTK

---
+++ src/app/store/public.ts
@@ -0,0 +1,2 @@
+export { useAppDispatch, useAppSelector } from "./hooks.private";
+export { store } from "./reducers.private";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from "react";
+import { signInGoogleWithPopup } from "@/shared/firebase/public";
 
 // Shared
 
@@ -8,6 +9,7 @@ import { ZnButton } from "@/shared/ui";
 // Icons
 
 import { MdPersonAdd } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
 
 const title = "What you focus on determines the quality of your life.";
 const description = `The service should remind, help, suggest, guide in different situations. Keep the user within the chosen values, conform to an
@@ -17,23 +19,35 @@ const Home: FC = () => {
   const [openMenu, setOpenMenu] = useState(false);
 
   const onToggleMenu = () => setOpenMenu(!openMenu);
+  const onAuthByGoogle = () => {
+    signInGoogleWithPopup();
+  };
 
   return (
     <MainLayout
       aside={
-        <ul>
-          {[
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-            20,
-          ].map((el) => (
-            <li
-              key={el}
-              className="p-2 hover:bg-gray-100 dark:hover:bg-zinc-900 cursor-pointer rounded-md"
-            >
-              {el} - test
-            </li>
-          ))}
-        </ul>
+        // <ul>
+        //   {[
+        //     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        //     20,
+        //   ].map((el) => (
+        //     <li
+        //       key={el}
+        //       className="p-2 hover:bg-gray-100 dark:hover:bg-zinc-900 cursor-pointer rounded-md"
+        //     >
+        //       {el} - test
+        //     </li>
+        //   ))}
+        // </ul>
+        <div className="flex justify-center items-center h-40">
+          <ZnButton
+            label="Continue with Google"
+            areaLabel="Use the button for Access to the site"
+            title="Get started to use the site"
+            icon={<FcGoogle className="icon" />}
+            onClick={onAuthByGoogle}
+          />
+        </div>
       }
       content={
         <>
