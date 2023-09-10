9862751901693a9c93ef18ae64dd9f615a34d0a9
avtandil <shainidze@gmail.com>
2023-09-10 11:20:29 +0300

    fix: clean all of data have binded with fitebase FCM

--- public/ws.js
+++ public/ws.js
@@ -1,20 +1,20 @@
-self.addEventListener("message", function (event) {
-  console.log("🚀 ~ file: ws.js:38 ~ event.data:", event.data.data);
-  setTimer(event.data.data);
-});
+// self.addEventListener("message", function (event) {
+//   console.log("🚀 ~ file: ws.js:38 ~ event.data:", event.data.data);
+//   setTimer(event.data.data);
+// });
 
-function setTimer(data) {
-  const intervalId = setInterval(() => {
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "https://aa10-37-73-86-230.ngrok-free.app/images/pwa-64x64.png",
-      renotify: true,
-      vibrate: [200, 100, 200, 100, 200, 100, 200],
-      tag: "vibration-sample",
-    });
-  }, 7000);
+// function setTimer(data) {
+//   const intervalId = setInterval(() => {
+//     self.registration.showNotification(data.title, {
+//       body: data.body,
+//       icon: "https://aa10-37-73-86-230.ngrok-free.app/images/pwa-64x64.png",
+//       renotify: true,
+//       vibrate: [200, 100, 200, 100, 200, 100, 200],
+//       tag: "vibration-sample",
+//     });
+//   }, 7000);
 
-  setTimeout(() => {
-    clearInterval(intervalId);
-  }, 35000);
-}
+//   setTimeout(() => {
+//     clearInterval(intervalId);
+//   }, 35000);
+// }
